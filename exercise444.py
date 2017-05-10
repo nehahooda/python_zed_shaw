1ba4914457f1ade6d4e94342227c32dc77cd12b0
nehahooda <nehahooda2012@gmail.com>
2017-05-10 22:12:29 +0530

    inheritance started

---
+++ exercise444.py
@@ -0,0 +1,13 @@
+class Parent(object):
+    def override(self):
+        print "PARENT OVERRIDE()"
+
+class Child(Parent):
+    def override(self):
+        print "CHILD OVERRIDE()"
+
+dad =Parent()
+son =Child()
+
+dad.override()
+son.override()
