35b063b33afff8a9b78685cdf7813e41d7e4abc6
nehahooda <nehahooda2012@gmail.com>
2017-06-25 20:46:48 +0530

    inheritance

---
+++ prac90.py
@@ -0,0 +1,28 @@
+class polygon:
+    def __init__(self,nos):
+        self.n=nos
+        self.sides=[0 for i in range(nos)]
+
+    def isides(self):
+        self.sides=[float (input("Enter side"+str(i+1)+":")) for i in range(self.n)]
+
+    def disp(self):
+        for i in range(self.n):
+            print("side",i+1,"is",self.sides[i])
+
+
+
+class Triangle(polygon):
+    def __init__(self):
+        polygon.__init__(self,3)
+    def Area(self):
+        a,b,c = self.sides
+        s= (a+b+c)/2
+        area = (s*(s-a)*(s-b)*(s-c))** 0.5
+        print("area %0.2f",area)
+
+
+t=Triangle()
+t.isides()
+t.disp()
+t.Area()
