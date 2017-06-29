1c449ac8da2978efd20c59bb65fb6b8945056736
nehahooda <nehahooda2012@gmail.com>
2017-06-29 21:43:23 +0530

    kk'

---
+++ prac95.py
@@ -0,0 +1,7 @@
+print "stuck in internship"
+print ("no help from anywhere")
+print "open door"
+print "open the door"
+print "go through the door"
+print "punch the bear"
+print "punch the bear in the face"
