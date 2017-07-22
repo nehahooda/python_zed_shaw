aa6517c0cd89d6256bb3889f937626a5eb7f728b
nehahooda <nehahooda2012@gmail.com>
2017-07-22 21:27:41 +0530

    Hr

---
+++ projects/HR/hr.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#read the dataset provided
+data= pd.read_csv('HR.csv')
+print("Total number of samples in file : ", data.shape[0])
+
+# Let's look at the data first
+print("A view of the  dataframe")
+print(data.head())
+print("\nColumns in  dataset : ", data.columns)
+print("\n")
+print("Overall description of the dataset : ")
+print(data.info())
+print("Overall numerical description of the data set")
+print(data.describe())
