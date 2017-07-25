695fa604d3fef08c907ac4f193b39ba332ebd034
nehahooda <nehahooda2012@gmail.com>
2017-07-25 23:25:03 +0530

    niit

--- NIIT/calls/CALL.py
+++ NIIT/calls/CALL.py
@@ -2,11 +2,9 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-#read the dataset providedn
+#read the dataset provided
 data= pd.read_csv('Call.csv')
-print("Total number of samples in file : ", data.shape[0])
-
+'''print("Total number of samples in file : ", data.shape[0])
 # Let's look at the data first
 print("A view of the  dataframe")
 print(data.head())
@@ -16,3 +14,23 @@ print("Overall description of the dataset : ")
 print(data.info())
 print("Overall numerical description of the data set")
 print(data.describe())
+'''
+
+#cleaning the dataset
+print(data.Date.nunique())
+del data['Date']
+print(data.Assist.nunique())
+del data['Assist']
+del data['LastCallWorkCode']
+
+
+#disposition plotting
+sns.countplot(x='Disposition',data =data )
+plt.show()
+
+sns.countplot(x='Segment',data =data )
+plt.show()
+
+sns.countplot(x='Skill',data =data )
+plt.show()
+
