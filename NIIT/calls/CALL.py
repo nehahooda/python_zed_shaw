644752db275375b119a864bb2f0da3c95669149f
nehahooda <nehahooda2012@gmail.com>
2017-07-26 23:04:17 +0530

    niit

--- NIIT/calls/CALL.py
+++ NIIT/calls/CALL.py
@@ -24,13 +24,51 @@ del data['Assist']
 del data['LastCallWorkCode']
 
 
-#disposition plotting
+
+'''
+#disposition plotting-what action was taken for the incoming calls
 sns.countplot(x='Disposition',data =data )
+plt.title("Disposition of incoming calls")
 plt.show()
 
-sns.countplot(x='Segment',data =data )
+#segment wise report
+seg = data['Segment'].value_counts()
+print(seg)
+f = plt.figure(figsize=(10,5))
+plt.axis("equal")
+plt.pie(seg, labels=seg.index,autopct="%1.1f%%")
+plt.title("Segment wise calls")
 plt.show()
 
+#showing the count of calls as per skills
 sns.countplot(x='Skill',data =data )
+plt.title("number of calls as per skills")
 plt.show()
 
+#showing the calls that were transfered out
+sns.countplot(x='TransferedOut',data =data )
+plt.title("Transferred out calls")
+plt.show()
+#showing calls that were conferenced in
+sns.countplot(x='ConferencedIn',data =data )
+plt.title("conferenced  In calls")
+plt.show()
+
+
+
+#looking at the disposition time wise skill
+sns.swarmplot(x='DispositionTime',y='Skill',data =data)
+
+plt.title("skills for disposition time")
+plt.xlabel("Disposition TIME")
+plt.ylabel("SKILLS")
+plt.grid()
+plt.show()
+
+
+
+
+#
+sns.swarmplot(x="TalkTime", y="Skill", data=data)
+plt.show()
+'''
