5bbe0e136496b0e6170c9e1b0d708a6db2e29247
nehahooda <nehahooda2012@gmail.com>
2017-08-26 18:56:16 +0530

    new beginnings

--- minor/mod1.py
+++ minor/mod1.py
@@ -1,11 +1,13 @@
 import numpy as np
+import pandas as pd
 from sklearn.cluster import KMeans
 
+df = pd.read_csv('menu.csv')
 ### For the purposes of this example, we store feature data from our
 ### dataframe `df`, in the `f1` and `f2` arrays. We combine this into
 ### a feature matrix `X` before entering it into the algorithm.
-f1 = df['Distance_Feature'].values
-f2 = df['Speeding_Feature'].values
-
-X=np.matrix(zip(f1,f2))
-kmeans = KMeans(n_clusters=2).fit(X)
+f1 = df['Total Fat'].values
+f2 = df['Saturated Fat'].values
+f3 = df['Trans Fat'].values
+X=np.matrix(zip(f1,f2,f3))
+kmeans = KMeans(n_clusters=3).fit(X)
